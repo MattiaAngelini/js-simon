31dcb4ed4e44a5c1c9e05ec24f4953286366bfb1
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-03-19 16:31:25 +0100

    Add: setTimeout on button play

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 
-//Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+//Visualizzare in pagina 5 numeri casuali. 
+//Da lì parte un timer di 30 secondi.
 //Dopo 30 secondi i numeri scompaiono 
 
 
@@ -9,29 +10,34 @@
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 //Consigli del giorno:
 
-
 //* Pensate prima in italiano.
 //* Dividete in piccoli problemi la consegna.
 //* Individuate gli elementi di cui avete bisogno per realizzare il programma.
 
-
 ///////START///////
-//creo prima funzione che genera 5 numeri casuali.
-let fiveCasualNumbers = generaNumeriCasualiUnici();
-console.log(fiveCasualNumbers);
+//Selettore del tasto play
+const buttonPlay = document.querySelector("#btn-play");
+//Selettore paragrafo destinato ai numeri
+const numbersDiv = document.querySelector("#numbers");
 
 
+//Click al tasto play che genera i 5 numeri della funzione per 30 secondi
+buttonPlay.addEventListener("click", function(){
+    let numbersArray = generateCasualNumbers();
+    numbersDiv.innerHTML= numbersArray;
+    setTimeout(() => numbersDiv.innerHTML = "", 3000)
 
-
-
-
-
+    //let playerNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+    
+    
+});
 
 
 
 //*********FUNCTION***********//
+
 // funzione che genera 5 numeri casuali unici da 1 a 100.
-function generaNumeriCasualiUnici(numbers) {
+function generateCasualNumbers() {
     let uniqueNumbers = [];
 
     while (uniqueNumbers.length < 5) {
@@ -43,3 +49,6 @@ function generaNumeriCasualiUnici(numbers) {
     return uniqueNumbers;
 }
 
+
+
+//funzione che farà comparire i numeri in un elemento <p>
